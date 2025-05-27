7d263ee15a40b2b29cd417e7e1317326d74acfb1
MuntasirAbid <muntasira7@hotmail.com>
2025-05-27 23:32:16 +0200

    refectored

---
+++ src/routes/admin.routes.tsx
@@ -0,0 +1,27 @@
+import AdminDashboard from "../pages/Admin/AdminDashboard";
+import CreateAdmin from "../pages/Admin/CreateAdmin";
+import CreateFaculty from "../pages/Admin/CreateFaculty";
+import CreateStudent from "../pages/Admin/CreateStudent";
+
+export const adminPaths = [
+  {
+    index: true,
+    element: <AdminDashboard></AdminDashboard>,
+  },
+  {
+    path: "dashboard",
+    element: <AdminDashboard></AdminDashboard>,
+  },
+  {
+    path: "create-student",
+    element: <CreateStudent></CreateStudent>,
+  },
+  {
+    path: "create-admin",
+    element: <CreateAdmin></CreateAdmin>,
+  },
+  {
+    path: "create-faculty",
+    element: <CreateFaculty></CreateFaculty>,
+  },
+];
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,44 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import Contact from "../pages/Contact";
-import About from "../pages/About";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
-import AdminDashboard from "../pages/Admin/AdminDashboard";
-import CreateStudent from "../pages/Admin/CreateStudent";
+import { adminPaths } from "./admin.routes";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-    ],
   },
   {
     path: "/admin",
     element: <App></App>,
-    children: [
-      {
-        index: true,
-        element: <AdminDashboard></AdminDashboard>,
-      },
-      {
-        path: "dashboard",
-        element: <AdminDashboard></AdminDashboard>,
-      },
-      {
-        path: "create-student",
-        element: <CreateStudent></CreateStudent>,
-      },
-    ],
+    children: adminPaths,
   },
   {
     path: "/register",
