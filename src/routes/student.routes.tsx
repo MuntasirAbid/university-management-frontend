bb25f9a140e555ed0632b3a1e8a16150aba104b9
MuntasirAbid <muntasira7@hotmail.com>
2025-05-31 22:39:54 +0200

    student route and faculty route

---
+++ src/routes/student.routes.tsx
@@ -0,0 +1,14 @@
+import StudentDashboard from "../pages/Student/StudentDashboard";
+
+export const studentPaths = [
+  {
+    name: "Dashboard",
+    path: "dashboard",
+    element: <StudentDashboard></StudentDashboard>,
+  },
+  {
+    name: "Offered course",
+    path: "offered-course",
+    element: "",
+  },
+];
